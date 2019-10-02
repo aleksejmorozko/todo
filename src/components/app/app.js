60d2d6d38bf2775914b7187e57d0c4519e2b839e
Aleksey <Aleksey@M>
2019-10-02 20:01:00 +0300

    lesson 047

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,7 @@ export default class App extends Component {
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch'),
     ],
-    dataToSee: []
+    term: '',
   };
   
   createTodoItem(label){
@@ -89,23 +89,35 @@ export default class App extends Component {
       }
     });
   };
+  search(items, term){
+    if (term.length === 0){
+      return items;
+    };
+    return items.filter(( item ) => {
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1
+    });
+  };
+  onSearchChange = (term) => {
+    this.setState({ term });
+  }
   render(){
    // {this.stateItem('all');}
-    const { todoData} = this.state;
+    const { todoData, term } = this.state;
+    const visibleItems = this.search( todoData, term );
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
     
     return (
       <div className="todo-app">
-        <AppHeader toDo={todoCount} done={doneCount} />
+        <AppHeader todo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
+          <SearchPanel onSearchChange={this.onSearchChange} />
           <ItemStatusFilter 
             stateItem = { this.stateItem } />
         </div>
   
         <TodoList 
-          todos = { todoData } 
+          todos = { visibleItems } 
           onDeleted = { this.deleteItem }
           onToggleImportant = { this.onToggleImportant }
           onToggleDone = { this.onToggleDone }
