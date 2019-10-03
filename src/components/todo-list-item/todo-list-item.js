6fceb2ee0f3df317f5699f541534c94c612e1888
Aleksey <Aleksey@M>
2019-10-03 23:31:14 +0300

    low mist 048

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react';
 import './todo-list-item.css';
 export default class TodoListItem extends Component{
-
-
   render(){
     const { label, onToggleImportant, onToggleDone, done, important } = this.props;
-
     let classNames = 'todo-list-item';
     if (done) {
       classNames+= ' done';
     }
-    if (important){classNames += ' important'}
-    
+    if (important){classNames += ' important'};    
     return (
       <span className={classNames}>
         <span
           className="todo-list-item-label"
           onClick={onToggleDone}>
           {label}
-        </span>
-  
+        </span>  
         <button type="button"
                 className="btn btn-outline-success btn-sm float-right"
                 onClick={onToggleImportant}>
           <i className="fa fa-exclamation" />
-        </button>
-  
+        </button>  
         <button type="button"
                 className="btn btn-outline-danger btn-sm float-right" onClick={this.props.onDeleted} >
           <i className="fa fa-trash-o" />
